d1aada7e3dff06769452ce253031f84e5c0e5f2b
gaussx <kmalecki2002@gmail.com>
2025-04-13 23:39:17 +0200

    Finish task 6 - ASP.NET MVC + Entity Framework (Code First)

---
+++ ASP.NET MVC + Entity Framework (Code First)/MvcNews/MvcNews/Migrations/20250403200642_AddRowVersionMig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcNews.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(140)",
+                maxLength: 140,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(140)",
+                oldMaxLength: 140);
+        }
+    }
+}
