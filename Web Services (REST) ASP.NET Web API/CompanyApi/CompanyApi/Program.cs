239519528ff217c9e9964e6092ba855b782def31
gaussx <kmalecki2002@gmail.com>
2025-04-23 16:05:43 +0200

    Finish task 8 - Web Services (REST): ASP.NET Web API

---
+++ Web Services (REST) ASP.NET Web API/CompanyApi/CompanyApi/Program.cs
@@ -0,0 +1,56 @@
+using CompanyApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<CompanyDbContext>(options =>
+                options.UseSqlServer(builder.Configuration
+                .GetConnectionString("CompanyDbContext")));
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            });
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
